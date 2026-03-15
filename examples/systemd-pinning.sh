d602df99e6b472c32ac4741ebbc4863d96087205
maxx1980 <96988611+maxx1980@users.noreply.github.com>
2026-03-15 21:30:38 +0200

    Update systemd-pinning.sh

--- examples/systemd-pinning.sh
+++ examples/systemd-pinning.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+SERVICES=(bird bird6 accel-ppp ssh sshd snmpd systemd-journald rsyslog cron)
+for SVC in "${SERVICES[@]}"; do
+    systemctl set-property "$SVC".service AllowedCPUs=8 9 10 11 || true
+done
 

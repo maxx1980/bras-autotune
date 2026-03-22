6e1842302509ac66c45cb5717778e206e87fc33f
maxx1980 <96988611+maxx1980@users.noreply.github.com>
2026-03-22 20:54:52 +0200

    Delete bras_autotune/ui/bkp directory

--- bras_autotune/ui/bkp/generate.py
+++
@@ -1,8 +0,0 @@
-from textual.screen import Screen
-from textual.widgets import Static
-
-class GenerateScreen(Screen):
-
-    def compose(self):
-        yield Static("Generate Configuration", id="title")
-        yield Static("TODO: implement config generator", id="gen")
--- bras_autotune/ui/bkp/nterfaces.py
+++
@@ -1,6 +0,0 @@
-from textual.screen import Screen
-from textual.widgets import Static
-
-class InterfacesScreen(Screen):
-    def compose(self):
-        yield Static("Interfaces screen (TODO)")

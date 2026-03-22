393bc5cf6deaffc53505e7503fc9a59dac1c0333
maxx1980 <96988611+maxx1980@users.noreply.github.com>
2026-03-22 17:43:34 +0200

    Delete debian_pkg directory

--- debian_pkg/usr/share/bras-autotune/bras_autotune/ui/bkp/app.py
+++
@@ -1,7 +0,0 @@
-from bras_autotune.ui.dashboard import DashboardApp
-
-
-def run_textual_ui():
-    app = DashboardApp()
-    app.run()
-    return None
--- debian_pkg/usr/share/bras-autotune/bras_autotune/ui/bkp/interface.py
+++
@@ -1,7 +0,0 @@
-from textual.screen import Screen
-from textual.widgets import Static
-
-class InterfacesScreen(Screen):
-    def compose(self):
-
-        yield Static("Interfaces screen (TOD0)")
